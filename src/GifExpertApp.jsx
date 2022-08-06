5e91809108d90adda8f354609f95e71183216b9d
Ezequiel Magi <ezequielmaggi@gmail.com>
2022-08-06 09:32:44 -0300

    Se creo la carpeta componentes y se acomodaron los archivos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import { AddCategory } from './AddCategory';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
